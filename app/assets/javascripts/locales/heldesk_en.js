63c814e42a0900ed4c68a586f1eac4a8f007e7b4
Akshay Sharma <cto@pixelsimple.com>
2013-02-21 01:39:45 -0500

    tickets page implementation

--- app/assets/javascripts/locales/heldesk_en.js
+++
@@ -1,9 +0,0 @@
-Em.I18n.translations = {
-    // Navigation
-    "leftnav.dashboard.title": "Dashboard",
-    "leftnav.tickets.title": "Tickets",
-    "leftnav.configure.title": "Configure",
-    "leftnav.admin.title": "Administration"
-
-
-};
--- app/router.js
+++ app/router.js
@@ -6,7 +6,7 @@ Helpdesk.Router.map(function() {
 	this.resource('dashboard', {paths: "/dashboard"});
 	
 	this.resource('tickets', function() {
-		this.resource('ticket', {path: ':ticket_category' });
+		this.route('ticket', {path: ':ticket_category' });
 	});
     this.resource('configure', {paths: "/configure"});
     this.resource('admin', {paths: "/admin"});
